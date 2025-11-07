a17bbfa74295a7179010e5e45f1408bff1f41966
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-07 02:08:28 -0400

    agregue es potgasto a GastoApi

---
+++ app/src/main/java/edu/ucne/liamell_cruz_ap2_p2/data/remote/dto/GastoApi.kt
@@ -0,0 +1,23 @@
+package edu.ucne.liamell_cruz_ap2_p2.data.remote.dto
+import edu.ucne.liamell_cruz_ap2_p2.data.remote.dto.GastoDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.POST
+import retrofit2.http.Body
+import retrofit2.http.PUT
+
+interface GastoApi {
+    @GET("api/Gastos")
+    suspend fun getGastos(): List<GastoDto>
+
+    @GET("api/Gastos/{id}")
+    suspend fun getGasto(@Path("id") id: Int): GastoDto
+
+    @POST("api/Gastos")
+    suspend fun postGasto(@Body gasto: GastoDto): GastoDto
+
+
+
+
+
+}
