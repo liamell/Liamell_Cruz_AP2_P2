9da91e9f3991468fe2037e7c1021b0c4e55b6165
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-07 02:03:27 -0400

    Correcion de datos en el GastoDto

--- app/src/main/java/edu/ucne/liamell_cruz_ap2_p2/data/remote/dto/GastoDto.kt
+++ app/src/main/java/edu/ucne/liamell_cruz_ap2_p2/data/remote/dto/GastoDto.kt
@@ -1,18 +1,13 @@
 package edu.ucne.liamell_cruz_ap2_p2.data.remote.dto
-import retrofit2.http.GET
-import retrofit2.http.Path
 
 data class GastoDto(
     val gastoId: Int,
-    val descripcion: String,
+    val fecha: String,
+    val suplidor: String,
+    val ncf: String,
+    val itbis: Double,
     val monto: Double
 )
 
-interface GastoApi {
 
-    @GET("Gastos")
-    suspend fun getGastos(): List<GastoDto>
 
-    @GET("Gastos/{id}")
-    suspend fun getGasto(@Path("id") id: Int): GastoDto
-}
