31a5eee6c1cce5ee65357f2f01d0b1618e260bf6
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-07 02:04:28 -0400

    Agregue el addgastos al repository

--- app/src/main/java/edu/ucne/liamell_cruz_ap2_p2/repository/GastoRepository.kt
+++ app/src/main/java/edu/ucne/liamell_cruz_ap2_p2/repository/GastoRepository.kt
@@ -1,11 +1,20 @@
 package edu.ucne.liamell_cruz_ap2_p2.repository
 import edu.ucne.liamell_cruz_ap2_p2.data.remote.dto.GastoApi
+import edu.ucne.liamell_cruz_ap2_p2.data.remote.dto.GastoDto
+//import edu.ucne.liamell_cruz_ap2_p2.presentation.navigation.RetrofitInstance.api
 import javax.inject.Inject
+
 
 class GastoRepository @Inject constructor(
     private val gastoApi: GastoApi
 ) {
-    suspend fun getGastos() = gastoApi.getGastos()
+    suspend fun getGastos(): List<GastoDto> = gastoApi.getGastos()
 
     suspend fun getGasto(id: Int) = gastoApi.getGasto(id)
+
+    suspend fun addGasto(gasto: GastoDto) = gastoApi.postGasto(gasto)
+
+
+
+
 }
