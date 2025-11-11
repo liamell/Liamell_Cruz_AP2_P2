b182ad598b97486c17761413a7ecf45c263c1dcd
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-11 00:59:18 -0400

    Cambio de direccion de import

--- app/src/main/java/edu/ucne/liamell_cruz_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/liamell_cruz_ap2_p2/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.liamell_cruz_ap2_p2.ui.gastos.GastoScreen
+import edu.ucne.liamell_cruz_ap2_p2.presentation.Screens.GastoScreen
 import edu.ucne.liamell_cruz_ap2_p2.ui.theme.Liamell_Cruz_AP2_P2Theme
 
 @AndroidEntryPoint
