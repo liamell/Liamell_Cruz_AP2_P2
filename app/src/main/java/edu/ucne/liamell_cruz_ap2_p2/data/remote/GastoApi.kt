423075dd348f475428db3bc3009794a33aad1b27
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-11 01:01:30 -0400

    Mejoras en el gastoapi

--- app/src/main/java/edu/ucne/liamell_cruz_ap2_p2/data/remote/GastoApi.kt
+++ app/src/main/java/edu/ucne/liamell_cruz_ap2_p2/data/remote/GastoApi.kt
@@ -1,23 +1,22 @@
 package edu.ucne.liamell_cruz_ap2_p2.data.remote
 
 import edu.ucne.liamell_cruz_ap2_p2.data.remote.dto.GastoDto
+import edu.ucne.liamell_cruz_ap2_p2.data.remote.dto.GastoRequest
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface GastoApi {
-    @GET("api/Gastos")
-    suspend fun getGastos(): List<GastoDto>
 
-    @GET("api/Gastos/{id}")
-    suspend fun getGasto(@Path("id") id: Int): GastoDto
+    @GET("api/Gastos")
+    suspend fun getGastos(): Response<List<GastoDto>>
 
     @POST("api/Gastos")
-    suspend fun postGasto(@Body gasto: GastoDto): GastoDto
+    suspend fun postGasto(@Body gasto: GastoRequest): Response<GastoDto>
 
-
-
-
-
+    @PUT("api/Gastos/{id}")
+    suspend fun updateGasto(@Path("id") id: Int, @Body gasto: GastoRequest): Response<Unit>
 }
