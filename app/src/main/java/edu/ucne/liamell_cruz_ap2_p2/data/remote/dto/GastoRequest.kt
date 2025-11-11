546de55b34a04f87f8a1e9d89abc3e507ce18ddc
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-11 01:00:18 -0400

    GastoRequest

---
+++ app/src/main/java/edu/ucne/liamell_cruz_ap2_p2/data/remote/dto/GastoRequest.kt
@@ -0,0 +1,9 @@
+package edu.ucne.liamell_cruz_ap2_p2.data.remote.dto
+
+data class GastoRequest(
+    val fecha: String,
+    val suplidor: String,
+    val ncf: String,
+    val itbis: Double,
+    val monto: Double
+)
